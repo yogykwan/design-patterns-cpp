192bde3b2134ffde687040b1e0dc66e155b3969c
Jing Guan <jingg.cxy@foxmail.com>
2016-12-26 17:15:07 +0800

    test: abstract-factory

--- tests/unit_tests/abstract_factory_test.cc
+++ tests/unit_tests/abstract_factory_test.cc
@@ -6,10 +6,47 @@
 #include "gtest/gtest.h"
 #include "abstract_factory.h"
 
-TEST(basic_check, test_eq) {
-  EXPECT_EQ(1, 1);
-}
+class AbstractFactoryFixture: public ::testing::Test {
+protected:
+  virtual void TearDown() {}
+  virtual void SetUp() {}
 
-TEST(basic_check, test_neq) {
-  EXPECT_NE(1, 0);
+public:
+  AbstractFactoryFixture() : Test() {
+    // use sqlserver to insert/get user/department
+    i_factory_ = new SqlserverFactory();
+
+    i_user_ = i_factory_->CreateUser();
+    i_user_->InsertUser(new User());
+    i_user_->GetUser(0);
+
+    i_department_ = i_factory_->CreateDepartment();
+    i_department_->InsertDepartment(new Department());
+    i_department_-> GetDepartment(0);
+
+    // use accecss to insert/get user/department
+    i_factory_ = new AccessFactory(); //compared with sqlserver abobve, only change here
+
+    i_user_ = i_factory_->CreateUser();
+    i_user_->InsertUser(new User());
+    i_user_->GetUser(0);
+
+    i_department_ = i_factory_->CreateDepartment();
+    i_department_->InsertDepartment(new Department());
+    i_department_-> GetDepartment(0);
+  }
+
+  virtual ~AbstractFactoryFixture() {
+    delete i_factory_;
+    delete i_user_;
+    delete i_department_;
+  }
+
+  IFactory* i_factory_;
+  IUser* i_user_;
+  IDepartment* i_department_;
+};
+
+TEST_F(AbstractFactoryFixture, abstract_factory_test) {
+  
 }
